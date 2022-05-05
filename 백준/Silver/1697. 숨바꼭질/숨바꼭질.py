86cb5bfd34d7d4484d0969315c13813df79f26f2
Jioo Jung <bluephph12@mju.ac.kr>
2022-05-05 14:35:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 34760 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+
+
+input = sys.stdin.readline 
+n,k = map(int,input().split())
+visited= [0]*(100001)
+
+def bfs():
+    q= deque()
+    q.append(n)
+    visited[n]= 1 
+    while q : 
+        x = q.popleft()
+        dx = [-1,1,x]
+        if x == k : 
+            return visited[x] -1 
+        for i in range(3):
+            nx = x + dx[i]
+            if 0 <= nx <=100000 and visited[nx] == 0 :
+                visited[nx] = visited[x]+1
+                q.append(nx)
+                
+
+print(bfs())
+
