8599b370b3679d24979ac95bcc15f3becd97dd6c
Jioo Jung <bluephph12@mju.ac.kr>
2022-05-17 13:33:58 +0900

    [Gold II] Title: K번째 수, Time: 1036 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,21 +1,21 @@
+
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
 
-start, end = 0, k
-# k번째 수는 k보다 클 수 없음
+start, end = 0, k # k번째 수는 k보다 클 수 없음
 
 while start <= end:
     mid = (start+end) // 2
     cnt = 0
 
     for i in range(1,n+1):
-        cnt += min(mid//i, n)  
-        
-    # print("[cnt]:",cnt)   
-    if cnt >= k:
+        cnt += min(mid//i, n)  # n x n 에서 mid 보다 작거나 같은 수
+    
+    if cnt >= k: # k 번째로 오는 수를 찾는다 
         answer = mid
         end = mid - 1
     else:
