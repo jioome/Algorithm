0e1117b7c9542b742f46c6d6a7bcb0ea35e32a5f
Jioo Jung <jioome17@gmail.com>
2022-08-15 19:12:57 +0900

    Time: 360 ms (43.27%), Space: 17.6 MB (94.80%) - LeetHub

--- LeetCode/1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ LeetCode/1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -4,23 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import math 
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         self.ans = 0 
-        self.result = 0 
+        self.depth = 0 
+        
         def find(root,height):
             if not root : 
-                return  
-            if not root.left and not root.right:
-                if self.result < height :
-                    self.result = height
-                    self.ans = root.val
-                elif  self.result == height :
-                    self.ans += root.val
                 return 
-            
+            if not root.left and not root.right : 
+                if self.depth < height : 
+                    self.depth = height
+                    self.ans = root.val
+
+                elif self.depth == height : 
+                    self.ans += root.val
+
             find(root.left,height+1)
             find(root.right,height+1)
+                
+        
         find(root,1)
         return self.ans
