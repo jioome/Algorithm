cbf84fbfcff862b1986d05fdf053593998638b41
Jioo Jung <jioome17@gmail.com>
2022-08-16 19:17:42 +0900

    Time: 476 ms (64.74%), Space: 16 MB (20.37%) - LeetHub

---
+++ LeetCode/77-combinations/77-combinations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+            
+        result = []
+        
+        def dfs(idx,lst):
+            
+            
+            if len(lst) == k:
+                result.append(lst)
+                print(1111)
+            
+            for i in range(idx+1,n+1):
+                dfs(i,lst+[i])
+                if i+k >n :
+                    continue
+                
+
+                
+        for i in range(1,n+1):
+            if k == 1 : 
+                result.append([i])
+            else : 
+                dfs(i,[i])
+        return result
