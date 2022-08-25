b543d54a7f4b915a7a24d466ee11274ef359f357
Jioo Jung <jioome17@gmail.com>
2022-08-25 12:33:34 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT a.animal_id, a.animal_type, a.name
+from animal_ins as a
+left join animal_outs as b 
+on a.animal_id = b.animal_id
+where a.sex_upon_intake != b.sex_upon_outcome
+order by a.animal_Id
