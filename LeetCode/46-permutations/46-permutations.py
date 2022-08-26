973ea9776b86f4a245be92d009a0fd5c6f8f43d4
Jioo Jung <jioome17@gmail.com>
2022-08-27 05:52:56 +0900

    Time: 58 ms (61.85%), Space: 13.8 MB (97.89%) - LeetHub

--- LeetCode/46-permutations/46-permutations.py
+++ LeetCode/46-permutations/46-permutations.py
@@ -1,9 +1,8 @@
-from itertools import *
-
+from itertools import * 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = [] 
-        for i in list(permutations(nums,len(nums))):
-            result.append(i)
-            
-        return result
+        p = list(permutations(nums,len(nums)))
+        print(p)
+        return p 
+        
