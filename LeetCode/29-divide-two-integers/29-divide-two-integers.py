d7280354a0d0b885e6251650a767f79a5d96b279
Jioo Jung <jioome17@gmail.com>
2022-09-01 11:18:00 +0900

    Time: 51 ms (57.01%), Space: 13.8 MB (76.19%) - LeetHub

--- LeetCode/29-divide-two-integers/29-divide-two-integers.py
+++ LeetCode/29-divide-two-integers/29-divide-two-integers.py
@@ -1,22 +1,9 @@
 class Solution:
-     def divide(self, dividend: int, divisor: int) -> int:
-        if dividend == 0:
-            return 0
-        if dividend == -2 ** 31 and divisor == -1:
-            return 2 ** 31 - 1
-			
-
-        if dividend > 0 and divisor > 0 or dividend < 0 and divisor < 0:
-            positiveSign = True
-        else:
-            positiveSign = False
-            
-        dividend, divisor = abs(dividend), abs(divisor)
-
-        quotient = 0
-        for i in reversed(range(dividend.bit_length() - divisor.bit_length() + 1)):
-            if dividend >> i >= divisor:
-                dividend -= divisor << i
-                quotient |= 1 << i
-        
-        return quotient if positiveSign else -quotient
+     def divide(self, A, B):
+        if (A == -2147483648 and B == -1): return 2147483647
+        a, b, res = abs(A), abs(B), 0
+        for x in range(32)[::-1]:
+            if (a >> x) - b >= 0:
+                res += 1 << x
+                a -= b << x
+        return res if (A > 0) == (B > 0) else -res
