07babc1c2e8cb53476e05d4692d019450c470ccc
Jioo Jung <jioome17@gmail.com>
2022-09-08 14:50:03 +0900

    [level 2] Title: 오픈채팅방, Time: 119.73 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,18 +1,19 @@
 def solution(record):
     answer = []
+    msg = {"Enter":"님이 들어왔습니다.", "Leave":"님이 나갔습니다."}
     name = {}
     for r in record : 
         r = r.split(' ')
-        if r[0] == 'Change' or r[0] == 'Enter' : 
+        if r[0] =="Enter" or r[0] == "Change":
             name[r[1]] = r[2]
-            
+    
     for r in record : 
         r = r.split(' ')
         if r[0] == 'Enter':
-            a = name[r[1]]
-            answer.append(a+"님이 들어왔습니다.") 
+            answer.append(name[r[1]]+msg[r[0]])
         if r[0] == 'Leave':
-            a = name[r[1]]
-            answer.append(a+"님이 나갔습니다.") 
+            answer.append(name[r[1]]+msg[r[0]])
+
+            
             
     return answer
