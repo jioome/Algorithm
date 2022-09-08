b7db122844530b192db6f3e3ca7b4437ea022c72
Jioo Jung <jioome17@gmail.com>
2022-09-08 15:42:48 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,31 +1,39 @@
-def change(n,k):
-    p = []
-    while n > k : 
-        # 몫과 나머지를 구하는 함수 divmod
-        n, mod = divmod(n,k)
-        p.append(str(mod))
-    p.append(str(n))
-    p = p[::-1]
-    p = ''.join(p)
-    return p 
-
-def isprime(n) :
-    if n == 1 : 
-        return False 
-    if n == 2 :
-        return True 
-    for i in range(2, int(n**0.5) + 1):
-        if n%i == 0  : 
+def isprime(x) : 
+    for i in range(2,int(x**0.5)+1) : 
+        if int(x)% i == 0 : 
             return False
-    return True 
-            
+    return True
     
-        
+    
 def solution(n, k):
-    num = change(n,k)
-    num = num.split('0')
-    cnt = 0
-    for n in num : 
-        if n and isprime(int(n)): 
-            cnt += 1 
-    return cnt
+    count = 0 
+    div = n//k
+    result = []
+    while div > 0:
+        result.append(n % k)
+        n = div
+        div = n//k
+    result.append(n % k)
+    result.reverse()
+    prime = []
+    p = ''
+    for r in result : 
+        if r == 0 : 
+            if len(p) > 0 and p!= '1': 
+                prime.append(p)
+                p = '' 
+            if p == '1' : 
+                p = ''
+            
+        else : 
+            p+= str(r)
+            
+    if len(p) > 0 and p!= '1': 
+                prime.append(p)
+    
+    for pr in prime : 
+        if isprime(int(pr)): 
+            count += 1 
+
+
+    return count
