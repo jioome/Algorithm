cdc69130d185c4ab28993b634d2549145c691f17
Jioo Jung <bluephph12@mju.ac.kr>
2022-09-08 11:23:00 +0900

    Update 메뉴 리뉴얼.py

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -6,13 +6,11 @@ def solution(orders, course):
     for c in course :
         lst = [] 
         for o in orders : 
+            # map 이용 / xy , yx 조합 같게 만들기 위해 sorted 
             lst+=list(map(''.join,(combinations(sorted(o),c))))
-    
         lst = Counter(lst)
-        print(lst)
         for a,b in lst.items() : 
             if b >=max(lst.values()) and b>=2 :
-                print(a)
                 answer.append(a)
             
 
