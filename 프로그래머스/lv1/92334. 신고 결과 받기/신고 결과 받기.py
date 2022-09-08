3c167c6fd9081558503c1a6ed722650af397199a
Jioo Jung <jioome17@gmail.com>
2022-09-08 14:35:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,32 +1,28 @@
 from collections import defaultdict
 def solution(id_list, report, k):
     answer = []
-    report= set(report)
-    cnt = defaultdict(int)
-    mail = defaultdict(list)
-    stop = [] 
-
+    report = set(report)
+    bad = defaultdict(int)
+    rman = defaultdict(list)
+    badlst = [] 
     for r in report : 
         a,b = r.split(' ')
-        cnt[b] += 1
-        mail[a].append(b)
-        
-
-    for c in cnt.items():
-        if c[1]>=k : 
-            stop.append(c[0])
-
-    if not stop : 
-        return [0]*len(id_list) 
+        bad[b] += 1 
+        rman[a] += [b]
     
+    for b,n in bad.items() : 
+        if n>=k : 
+            badlst.append(b)
+    if len(badlst) == 0 :
+        answer = [0]*len(id_list)
+        return answer 
+
 
     for i in id_list : 
-        cnt = 0 
-        for m in mail[i]:
-            if m in stop : 
-                cnt += 1 
+        cnt= 0 
+        for a in rman[i]:
+            if a in badlst : 
+                cnt +=1 
         answer.append(cnt)
-        
-        
-        
+            
     return answer
