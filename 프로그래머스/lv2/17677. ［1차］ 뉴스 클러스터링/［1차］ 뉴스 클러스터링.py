384ff68f241a8435823d624a78733c4c386c94ed
Jioo Jung <jioome17@gmail.com>
2022-09-10 22:32:44 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+from collections import Counter 
+def solution(str1, str2):
+    answer = 0
+    str1 = str1.lower()
+    str2 = str2.lower()
+    a = [] 
+    b = [] 
+    for i in range(len(str1)-1):
+        if str1[i].isalpha() and str1[i+1].isalpha():
+            a.append(str1[i] + str1[i+1])
+    for i in range(len(str2)-1):
+        if str2[i].isalpha() and str2[i+1].isalpha():
+            b.append(str2[i] + str2[i+1])
+            
+    ca = Counter(a)
+    cb = Counter(b)
+    inter = list((ca & cb).elements())
+    union = list((ca | cb).elements())
+    if len(union) == 0 and len(inter) == 0:
+        return 65536
+    else:
+        return int(len(inter) / len(union) * 65536)
+    
