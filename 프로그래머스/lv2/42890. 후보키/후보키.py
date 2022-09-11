89f6cd1f717ba245d2fe1b6e1855f1bc3e442df7
Jioo Jung <bluephph12@mju.ac.kr>
2022-09-11 18:48:26 +0900

    Update 후보키.py

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,27 +1,30 @@
 from itertools import combinations
+def solution(relation):
+    answer = 0
+    col = len(relation[0])
+    row = len(relation)
+    combi = [] 
+    # 후보키 가능 경우의 수 
+    for i in range(1,col+1):
+        combi+= combinations(range(col),i)
 
-def solution(relations):
-    row, col = len(relations), len(relations[0])
-    candidates = []
-
-    for i in range(1, col + 1):
-        candidates.extend(combinations(range(col), i))
-
-    unique = []
-    for candidate in candidates:
-        temp = [tuple([relation[i] for i in candidate]) for relation in relations]
-
-        # 유일성 보장 
-        if len(set(temp)) == row:
-            check = True
-            
-            for u in unique:
-                # u 가 candidate에 속해 있는지 
-                if set(u).issubset(set(candidate)):
+    # 유일성
+    unique = [] 
+    
+    for com in combi :
+        only = [] 
+        for r in relation : 
+            only+= [r[c] for c in com ]
+        if len(set(only)) == row : 
+            check = True 
+            for x in unique :
+                # 최소성 확인 
+                if set(x).issubset(set(com)):
                     check = False
                     break
-            
-            if check:
-                unique.append(candidate)
-
+            if check : 
+                unique.append(com)
+        
+        
+        
     return len(unique)
