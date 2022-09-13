e71e8036faa05c83b0e733547f63762ac5cad385
Jioo Jung <jioome17@gmail.com>
2022-09-13 17:16:18 +0900

    [level 2] Title: 괄호 변환, Time: 0.17 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -1,61 +1,50 @@
-def divided(p):
-    cnt = 0
-    for i in range(len(p)) : 
-        if p[i] =='(' : 
-            cnt += 1 
-        else :
-            cnt -= 1 
-        if cnt == 0 :
-            u = p[:i+1]
-            v = p [i+1 : ]
-            return u,v
-    
-# 올바른 문자열인지 확인 
-def check_balance(u) : 
-    cnt = 0
-    for i in u :  
-        if i =='(' : 
-            cnt += 1 
+def right(u) : 
+    r = 0
+    for j in u : 
+        if j == ')':
+            r -= 1
         else : 
-            cnt -=1 
-
-        if cnt < 0 :
+            r += 1 
+        if r < 0 : 
             return False
     return True
-#     stack = []
-
-#     for p in u:
-#         if p == '(':
-#             stack.append(p)
-#         else:
-#             if not stack:
-#                 return False
-#             stack.pop()
-
-#     return True
+        
 
 def solution(p):
-    if not p :
+    answer = ''
+    # 1 
+    if len(p) == 0: 
         return ''
-    u, v = divided(p)
-    
-    if check_balance(u) : 
+
+    # 2 
+    cnt = 0
+    u = ''
+    v = ''
+    # 균형
+    for i in range(len(p)) :
+        if p[i] =='(' : 
+            cnt += 1 
+            u += p[i]
+        else : 
+            cnt -= 1
+            u += p[i]
+        if cnt == 0 :
+            v = p[i+1:]
+            break
+    # 이 부분이 헷갈림 ,, 
+    if right(u) : 
         return u + solution(v)
     else : 
-# 올바른 문자열로 만들기 
-        # 과정 4-1
-        answer = '('
-        # 과정 4-2
+        answer += '('
         answer += solution(v)
-        # 과정 4-3
         answer += ')'
-
-        # 과정 4-4
-        for p in u[1:len(u) - 1]:
-            if p == '(':
+        u = u[1:-1]
+        for j in u : 
+            if j == '(':
                 answer += ')'
-            else:
+            else : 
                 answer += '('
-            
-            
+    
+                
+
     return answer
