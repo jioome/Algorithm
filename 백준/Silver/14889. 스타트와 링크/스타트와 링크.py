1d7d1d802a1c09494f0cd6a8ec5f70dd24a36f69
Jioo Jung <bluephph12@mju.ac.kr>
2022-09-28 12:44:32 +0900

    Update 스타트와 링크.py

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -3,7 +3,8 @@ n = int(input())
 board = [list(map(int,input().split()))for _ in range(n)]
 member= list(range(n))
 result = float('inf')
-# 능력차 최소 
+
+# 능력차 최소가 되게 하기 
 
 coms = list(combinations(range(n),n//2))
 for com in coms : 
@@ -13,8 +14,10 @@ for com in coms :
     # 스타트팀
     for a,b in combinations(com,2):
         sum1 += board[a][b] + board[b][a]
+    # 링크팀 구하기 
     for c in com:
         member.remove(c)
+        
     for a,b in combinations(member,2):
         sum2 += board[a][b] + board[b][a]
     
