62ec06efd17cf214c13aed061bbfc9f7484c33bc
Jioo Jung <jioome17@gmail.com>
2022-09-28 11:38:15 +0900

    [Silver II] Title: 스타트와 링크, Time: 5204 ms, Memory: 55976 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,26 @@
+from itertools import * 
+n = int(input())
+board = [list(map(int,input().split()))for _ in range(n)]
+member= list(range(n))
+result = float('inf')
+# 능력차 최소 
+
+coms = list(combinations(range(n),n//2))
+for com in coms : 
+    member= list(range(n))
+    sum1 = 0 
+    sum2 = 0 
+    # 스타트팀
+    for a,b in combinations(com,2):
+        sum1 += board[a][b] + board[b][a]
+    for c in com:
+        member.remove(c)
+    for a,b in combinations(member,2):
+        sum2 += board[a][b] + board[b][a]
+    
+    result = min(result,abs(sum1-sum2))
+    
+print(result)   
+
+
+    
