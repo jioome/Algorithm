0e33631bbaf859c0dd04d4479c7d30f5710cb8e9
Jioo Jung <jioome17@gmail.com>
2022-09-29 19:30:43 +0900

    [Gold III] Title: 다리 만들기, Time: 160 ms, Memory: 32556 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,20 +1,25 @@
-import sys
 from collections import deque
+import sys
+input = sys.stdin.readline 
 
-input = sys.stdin.readline
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
-dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-distance = [[-1] * N for _ in range(N)]     # 거리 리스트
-sea = deque([])     # 육지 가장자리 x, y 좌표와 육지 번호 저장
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(n)]
+q = deque([])
+visited = [[0]*n for _ in range(n)]
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
 
+# 거리 리스트 
+distance = [[-1] * n for _ in range(n)]
+
+# 육지 가장자리(바다) x,y 좌표와 육지 번호 저장
+sea = deque([])
 
 # 육지 넘버링
 def numbering(board, number):
     q = deque([])
-    for i in range(N):
-        for j in range(N):
+    for i in range(n):
+        for j in range(n):
             if board[i][j] == 1 and not visited[i][j]:
                 q.append((i, j))
                 visited[i][j] = True
@@ -24,9 +29,9 @@ def numbering(board, number):
                 while q:
                     r, c = q.popleft()
                     for d in range(4):
-                        nr = r + dir[d][0]
-                        nc = c + dir[d][1]
-                        if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]:
+                        nr = r + dx[d]
+                        nc = c + dy[d]
+                        if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc]:
                             # 육지라면 넘버링 계속 진행
                             if board[nr][nc] == 1:
                                 q.append((nr, nc))
@@ -44,31 +49,27 @@ def numbering(board, number):
                 number += 1
 
 
-# 다리 놓기
+# 다리 놓기 
 def extend(board):
-    answer = 1e9
-    while sea:
-        r, c, bridge = sea.popleft()
+    answer = float('inf')
+    while sea :
+        x,y,bridge = sea.popleft()
         for i in range(4):
-            nr = r + dir[i][0]
-            nc = c + dir[i][1]
-            if 0 <= nr < N and 0 <= nc < N:
-                # 바다라면 다리를 놓아준다.
-                if board[nr][nc] == 0:
-                    # 육지번호가 bridge 인 육지에서 다리를 설치한다는 뜻
-                    board[nr][nc] = bridge
-                    distance[nr][nc] = distance[r][c] + 1   # 다리길이 + 1
-                    sea.append((nr, nc, bridge))
-
-                # 다른 육지에서 놓은 다리와 만날경우 거리 계산
-                # 즉, 섬과 섬이 이어졌다는 뜻
-                elif board[nr][nc] != bridge:
-                    answer = min(answer, distance[r][c] + distance[nr][nc])
-                    # flag 변수를 만들어서 강제로 while 문 종료시켜도 될 듯하다.
-                    # --> 섬과 섬을 잇는 최초의 다리는 가장 짧은 다리이기 때문이다.
-
-    return answer
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<= nx < n and 0 <= ny < n :
+                # 바다라면 다리 설치
+                if board[nx][ny] == 0 : 
+                    board[nx][ny] = bridge
+                    distance[nx][ny] = distance[x][y] + 1 
+                    sea.append((nx,ny,bridge))
+                
+                # 섬과 섬이 이어졌을 떄 , 다른 다리와 만날 때
+                elif board[nx][ny] != bridge:
+                    answer = min(answer,distance[x][y]+distance[nx][ny])
+    return answer 
 
 
-numbering(board, 1)
+    
+numbering(board,1)
 print(extend(board))
