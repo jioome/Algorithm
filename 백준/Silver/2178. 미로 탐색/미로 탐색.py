36c1fb93a9ce39ef0e05addd713380b05ddc169f
Jioo Jung <bluephph12@mju.ac.kr>
2022-09-29 11:00:43 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,3 @@
-from collections import deque
-n,m  = map(int,input().split())
-board = [list(map(int,input())) for _ in range(n)]
-visited = [[0]*m for _ in range(n)]
-
-q = deque()
-def bfs(a,b) : 
-    dx = [ -1,1,0,0]
-    dy = [0,0,1,-1]
-    q.append((a,b))
-    visited[a][b] = 1 
-    while q : 
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if  0<= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and board[nx][ny] == 1:
-                board[nx][ny] = board[x][y] + 1 
-                q.append((nx,ny))
-            
-
-bfs(0,0)
-print(board[n-1][m-1])
 
 from collections import deque
 n,m  = map(int,input().split())
