cfe7de9fb1a4fff242c479cc5e9d0f610a1e6779
Jioo Jung <bluephph12@mju.ac.kr>
2022-09-30 17:58:10 +0900

    Update 단지번호붙이기.py

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,32 @@
+import sys
+sys.setrecursionlimit(10**5)
 n = int(input())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int,input())))
- 
-grp = []
-cnt = 0
-dx = [-1,1,0,0] # 상하좌우
-dy = [0,0,-1,1]
- 
+board = [list(map(int,input())) for _ in range(n)]
+result = []
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+house =0
 def dfs(x,y):
-    global cnt
-    if x<0 or x>=n or y<0 or y>=n: # 범위
-        return False
-    
-    if graph[x][y]==1:
-        cnt +=1
-        graph[x][y] = 0
-        for i in range(4):
-            dfs(x+dx[i],y+dy[i])
-        return True
-    
-    
-for i in range(n):
+    # 한 단지마다 집의 수 
+    global house 
+    house+= 1 
+    # 값을 0으로 바꿔주면서 방문 처리 
+    board[x][y] = 0 
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 1: 
+            dfs(nx,ny)
+        
+for i in range(n): 
     for j in range(n):
-        if dfs(i,j)==True:
-            grp.append(cnt)
-            cnt = 0
-            
-print(len(grp))
-grp.sort()
-for i in grp:
-    print(i)
+        if board[i][j] == 1  : 
+            dfs(i,j)
+            result.append(house)
+            house =0
+        
+result.sort()
+print(len(result))
+for r in result : 
+    print(r)
