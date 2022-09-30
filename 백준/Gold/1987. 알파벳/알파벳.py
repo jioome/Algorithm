19ceefc570daaf1e9f64393af06e858d2d0729f6
Jioo Jung <bluephph12@mju.ac.kr>
2022-09-30 17:20:28 +0900

    Update 알파벳.py

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,29 +1,28 @@
 import sys
-from collections import deque
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-R,C=map(int,input().split())
-arr=[list(input()) for _ in range(R)]
-check=[['']*C for _ in range(R)]
-l=[(0,0,1,arr[0][0])]
-MAX=0
-dx=[-1,0,1,0]
-dy=[0,1,0,-1]
-while l:
-    x,y,cnt,total=l.pop()
-    if MAX<cnt:
-        MAX=cnt
-    if MAX==26:
-        break
+r,c = map(int,input().split())
+board = [list(input()) for _ in range(r)]
+# 시간 초과 때문에 set 사용
+check = set()
+ans = 0 
+# 몇 칸 갈 수 있는지
+result = []
+def dfs(x,y,cnt):
+    global ans
+    ans = max(ans,cnt)
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    check.add(board[x][y])
     for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if 0<=nx<R and 0<=ny<C:
-            if arr[nx][ny] not in total:
-                temp=total+arr[nx][ny]
-                if check[nx][ny]!=temp:
-                    check[nx][ny]=temp
-                #print(check,l)
-                    l.append((nx,ny,cnt+1,temp))
-print(MAX)
-    
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < r and 0 <= ny < c  : 
+            if board[nx][ny] not in check:
+                dfs(nx,ny,cnt+1)
+    check.remove(board[x][y])
+
+        
+
+dfs(0,0,1)
+print(ans)
