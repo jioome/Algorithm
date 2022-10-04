e8545f4da3ddcfabbd4ccd911d40a151141eab85
Jioo Jung <jioome17@gmail.com>
2022-10-04 16:23:24 +0900

    [Gold V] Title: 동전 2, Time: 428 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,19 @@
+import sys 
+input = sys.stdin.readline 
+
+n,k = map(int,input().split())
+graph = [] 
+for i in range(n):
+    graph.append(int(input()))
+dp = [10001]*(k+1)
+
+dp[0] = 0
+
+for num in graph : 
+    for i in range(num,k+1):
+        dp[i] = min(dp[i],dp[i-num]+1)
+
+if dp[k] == 10001 : 
+    print(-1)
+else : 
+    print(dp[k])
