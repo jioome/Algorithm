8034bb586ee793ec21e7b47a68be82b2272cfd0b
Jioo Jung <jioome17@gmail.com>
2022-10-10 12:14:59 +0900

    [Gold V] Title: 피자 굽기, Time: 2108 ms, Memory: 77212 KB -BaekjoonHub

---
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.py
@@ -0,0 +1,41 @@
+import sys 
+
+# 오븐 깊이 , 반죽 개수
+d,n = map(int,input().split())
+# 오븐 지름 
+oven = list(map(int,input().split()))
+# 피자 지름 
+doughs = list(map(int,input().split()))
+
+for i in range(1,len(oven)):
+    if oven[i]>oven[i-1]:
+        oven[i] = oven[i-1]
+
+# 도우가 어디에 쌓이는지
+pile_d = 0 
+l = 0 
+r = len(oven)-1
+for d in doughs:
+    is_pile = False
+    while l <= r : 
+        mid = (l + r)// 2 
+        diameter = oven[mid]
+        
+        if diameter >= d :
+            l = mid + 1
+            is_pile = True
+            pile_d = mid
+        else :
+            r = mid-1
+
+    if not is_pile:
+        pile_d -= 1
+        break
+    l = 0 
+    r = pile_d-1
+
+if pile_d == -1 : 
+    print(0)
+else : 
+    print(pile_d+1)
+ 
