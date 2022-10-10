027eb89793e5df843ab8ff28324481b06232621e
Jioo Jung <jioome17@gmail.com>
2022-10-10 11:14:31 +0900

    [Silver V] Title: 숫자 카드, Time: 2304 ms, Memory: 110408 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,32 +1,28 @@
 import sys
-
-
-input = sys.stdin.readline
-
-
+sys.setrecursionlimit(10**9)
 n = int(input())
-card = list(map(int, sys.stdin.readline().split()))
-m = int(input())
-check = list(map(int, sys.stdin.readline().split()))
-
+card = list(map(int,input().split()))
 card.sort()
-
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return False
+m = int(input())
+lst = list(map(int,input().split()))
+answer = [] 
 
 
-for i in range(m):
-    # check[i]가 card안에 있는지 확인 
-    if binary_search(card, check[i], 0, n - 1) is not False: 
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+
+def binary_search(c): 
+    left = 0 
+    right = n-1
+    while left <= right : 
+        mid = (left+right)//2
+        
+        if card[mid] > c : 
+            right = mid-1 
+        elif card[mid] < c : 
+            left = mid + 1
+        else : 
+            return 1
+    return 0 
+
+for c in lst : 
+    answer.append(str(binary_search(c)))
+print(' '.join(answer))
