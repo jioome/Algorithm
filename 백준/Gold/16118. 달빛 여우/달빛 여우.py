2c286b5099acfa201ec07e9b936105ec7f24c4b7
Jioo Jung <bluephph12@mju.ac.kr>
2022-10-12 10:07:17 +0900

    Update 달빛 여우.py

--- 백준/Gold/16118. 달빛 여우/달빛 여우.py
+++ 백준/Gold/16118. 달빛 여우/달빛 여우.py
@@ -1,68 +1,72 @@
-from heapq import heappush, heappop
+import heapq
 import sys
-rl = sys.stdin.readline
-INF = int(1e9)
+input = sys.stdin.readline
+InF = float('inf')
 
 
 def dik_fox():
-    dist = [INF for _ in range(N+1)]
+    dist = [InF]*(n+1)
     dist[1] = 0
-    q = []
-    heappush(q, (dist[1], 1))
+    q = [] 
+    heapq.heappush(q,(dist[1],1))
 
-    while q:
-        cd, ci = heappop(q)
-        if dist[ci] < cd:
+    while q : 
+        d,c = heapq.heappop(q)
+        if dist[c] < d : 
             continue
-        for vd, vi in G[ci]:
-            nd = cd + vd
-            if nd < dist[vi]:
-                dist[vi] = nd
-                heappush(q, (dist[vi], vi))
+        for d1,c1 in g[c]:
+            sd = d1 + d
+            if dist[c1] > sd : 
+                dist[c1] = sd 
+                heapq.heappush(q,(sd,c1))
     return dist
 
 
 def dik_wolf():
     # dist[0] 빠르게 도착 / dist[1] 느리게 도착
-    dist = [[INF] * (N+1) for _ in range(2)]
+    dist = [[InF] * (n+1) for _ in range(2)]
+    # 시작은 빠르게 달림 / 느린 거는 0 
     dist[1][1] = 0
     q = []
-    heappush(q, (dist[1][1], 1, False))
+    heapq.heappush(q,(dist[1][1],1,False))
 
+    # 빠르게 달릴 차례 -> false
+    # 느리게 달릴 차례 -> True 
     while q:
-        cd, ci, cf = heappop(q)
+        cd, ci, cf = heapq.heappop(q)
         if cf and dist[0][ci] < cd:
             continue
         elif not cf and dist[1][ci] < cd:
             continue
 
-        for vd, vi in G[ci]:
+        for vd, vi in g[ci]:
             if cf:  # 빠르게 도착했다면, 느리게 출발
                 nd = cd + (vd * 2)
                 if nd < dist[1][vi]:
                     dist[1][vi] = nd
-                    heappush(q, (dist[1][vi], vi, False))
+                    heapq.heappush(q, (dist[1][vi], vi, False))
             else:  # 느리게 도착했다면, 빠르게 출발
                 nd = cd + (vd // 2)
                 if nd < dist[0][vi]:
                     dist[0][vi] = nd
-                    heappush(q, (dist[0][vi], vi, True))
+                    heapq.heappush(q, (dist[0][vi], vi, True))
 
     return dist
 
 
-N, M = map(int, rl().split())
-G = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b, d = map(int, rl().split())
-    G[a].append((d * 2, b))
-    G[b].append((d * 2, a))
+n, m = map(int, input().split())
+g = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, d = map(int, input().split())
+    # 계산을 정수로만 하기 위해 길이에 전부 2를 곱해줌
+    g[a].append((d * 2, b))
+    g[b].append((d * 2, a))
 
 fox = dik_fox()
 wolf = dik_wolf()
 
 answer = 0
-for i in range(1, N+1):
+for i in range(1, n+1):
     if fox[i] < min(wolf[0][i], wolf[1][i]):
         answer += 1
 print(answer)
