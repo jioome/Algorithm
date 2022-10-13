17f478b631b042247ae33f76081e027108324d6f
Jioo Jung <bluephph12@mju.ac.kr>
2022-10-14 01:58:08 +0900

    Update 장난감 조립.py

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -22,10 +22,14 @@ while q :
     for next,next_need in graph[now]:
         # 현 제품이 기본 부품이면
         if needs[now].count(0) == n+1:
+            # 다음 부품 개수 구하기 
             needs[next][now] += next_need
         else : 
             # 현 제품이 중간 제품이면
             for i in range(1,n+1):
+                # next= 7 i= 1 now = 5 i = 1 needs = 2
+                # 5번 중간 부품 만드는데 1번 부품 2개 필요
+                # 7번 만드는데 5번 중간 부품 2개 필요  -> 총 2*2
                 needs[next][i] += needs[now][i]*next_need
         indegree[next] -= 1
         if indegree[next] == 0 : 
