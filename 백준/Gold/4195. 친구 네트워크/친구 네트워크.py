bc6f3a289ad0948917fce36951d8afae78e09553
Jioo Jung <jioome17@gmail.com>
2022-10-14 11:51:48 +0900

    [Gold II] Title: 친구 네트워크, Time: 352 ms, Memory: 66688 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -1,39 +1,38 @@
-# Root node를 찾아주는 함수
-def find(x):
-    if x == parent[x]:
-        return x
-    else:
-        root_x = find(parent[x])
-        parent[x] = root_x
-        return parent[x]
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+n = int(input())
 
-# y의 Root 노드가 x의 Root 노드와 같지 않으면 
-# y의 Root 노드가 x의 Root 노드의 자식이 되도록 하는 함수
+# root 노드 찾기
+def find(x):
+    if parent[x] != x : 
+        parent[x] = find(parent[x])
+        return parent[x]
+    else : 
+        return x
+
+# root 노드 합치기 
 def union(x,y):
     root_x = find(x)
     root_y = find(y)
-
-    if root_x!=root_y:
+    if root_x != root_y :
         parent[root_y] = root_x
-        number[root_x] +=number[root_y]
+        number[root_x]+= number[root_y]
+        
 
-test_cases = int(input())
-
-for _ in range(test_cases):
+for i in range(n):
     parent = dict()
     number = dict()
-
     f = int(input())
-
-    for _ in range(f):
-        x,y = input().split(" ")
-        
-        if x not in parent:
+    for j in range(f):
+        x,y = input().split()
+        # 자기 자신 부모로 초기화 
+        if x not in parent : 
             parent[x] = x
             number[x] = 1
         if y not in parent:
-            parent[y] = y
+            parent[y] = y 
             number[y] = 1
         
-        union (x,y)
+        union(x,y)
         print(number[find(x)])
