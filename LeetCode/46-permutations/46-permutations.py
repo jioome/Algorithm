a64b15c7761fe303f660c0242ace38f1dbd51b00
Jioo Jung <jioome17@gmail.com>
2022-10-18 00:18:54 +0900

    Time: 47 ms (86.27%), Space: 14.1 MB (23.42%) - LeetHub

--- LeetCode/46-permutations/46-permutations.py
+++ LeetCode/46-permutations/46-permutations.py
@@ -1,8 +1,15 @@
-from itertools import * 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = [] 
-        p = list(permutations(nums,len(nums)))
-        print(p)
-        return p 
-        
+        result =[] 
+        pre = [] 
+        def dfs(nums):
+            if len(nums)== 0 : 
+                result.append(pre[:])
+            for n in nums : 
+                nex = nums[:]
+                nex.remove(n)
+                pre.append(n)
+                dfs(nex)
+                pre.pop()
+        dfs(nums)
+        return result
