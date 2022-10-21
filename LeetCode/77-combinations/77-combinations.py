c5a21b66ecaebd8af6061d60bd9ab0ff72fd7959
Jioo Jung <jioome17@gmail.com>
2022-10-21 14:57:55 +0900

    Time: 1207 ms (20.47%), Space: 15.8 MB (85.64%) - LeetHub

--- LeetCode/77-combinations/77-combinations.py
+++ LeetCode/77-combinations/77-combinations.py
@@ -1,13 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        arr = [i for i in range(n+1)]
-        result = [] 
-        def dfs(idx,k,lst):
+        answer = [] 
+        def dfs(k,lst,idx):
             if k == 0 :
-                result.append(lst)
+                answer.append(lst[:])
             for i in range(idx,n+1):
-                dfs(i+1,k-1,lst+[arr[i]])
-            
-        dfs(1,k,[])
-        return result
+                dfs(k-1,lst + [i] , i + 1)
+                
+        
+        dfs(k,[],1)
+        return answer
+        
         
