d583e34bd973a688d24dc75e649e9f3813c8b00f
Jioo Jung <jioome17@gmail.com>
2022-10-25 01:47:20 +0900

    [Gold II] Title: 우수 마을, Time: 100 ms, Memory: 43816 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.py
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -1,25 +1,29 @@
-import sys, collections
+import sys 
 sys.setrecursionlimit(10**6)
+input = sys.stdin.readline 
 
+n = int(input())
+graph= [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+
+
+# 인접 x , 양방향 
+cost = [0] + list(map(int,input().split()))
+dp= [[0,cost[i]] for i in range(n+1)]
+for i in range(n-1):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
 def dfs(cur):
-    visited[cur] = 1
-    for u in g[cur]:
+    visited[cur] = 1 
+    for u in graph[cur]:
         if not visited[u]:
             dfs(u)
-            dp[cur][1] += dp[u][0] # 현재 마을을 우수마을로 선정
-            dp[cur][0] += max(dp[u][0], dp[u][1]) # 현재 마을을 우수마을로 선정 X
-
-n = int(sys.stdin.readline().strip())
-cost = [0] + [int(x) for x in sys.stdin.readline().split()]
-
-visited = [0 for _ in range(n+1)]
-dp = [[0, cost[i]]*2 for i in range(n+1)] # dp[i][0] = i마을을 선정X, dp[i][1] = i마을을 선정O
-g = collections.defaultdict(list)
-
-for _ in range(n-1):
-    v, u = map(int, sys.stdin.readline().split())
-    g[v].append(u)
-    g[u].append(v)
+            dp[cur][1] += dp[u][0]
+            dp[cur][0] += max(dp[u][0],dp[u][1])
 
 dfs(1)
-print(max(dp[1][1], dp[1][0]))
+print(max(dp[1][1],dp[1][0]))
+
+
