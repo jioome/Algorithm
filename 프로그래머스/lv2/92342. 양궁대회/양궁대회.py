d5d051d3e44bb4b1df3ae549ec3ce1452e5c3dfa
Jioo Jung <jioome17@gmail.com>
2022-10-24 12:54:55 +0900

    [level 2] Title: 양궁대회, Time: 430.91 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,29 +1,27 @@
 from itertools import combinations_with_replacement as cwr
-
 def solution(n, info):
     answer = [-1]
     maxgap = -1
-    
-    for combi in cwr(range(11),n):
-        # 라이언
-        info2 = [0]*11
-        apeach, lion = 0,0
+    for com in cwr(range(11),n):
+        info2= [0]*11
+        # 이긴 횟수 
+        lion,apeach = 0,0
         
-        for i in combi:
-            info2[10-i] += 1
-        
-        for idx in range(11):
-            if info[idx] == info2[idx] == 0 : 
+        for i in com : 
+            info2[10-i]+=1
+            
+        for i in range(11):
+            if info[i] == info2[i] == 0 : 
                 continue
-            elif info[idx]>=info2[idx]: # 어피치가 점수 높음
-                apeach += 10-idx
-            else :  # 라이언이 점수 높음 
-                lion += 10 - idx
-                
-        if lion > apeach : # 라이언이 이김 
-            gap = lion- apeach
-            if gap > maxgap:
+            elif info[i] >= info2[i] : # 어피치가 이김 
+                apeach += 10-i 
+            else : 
+                lion += 10-i
+        if lion > apeach :
+            gap = lion-apeach
+            if maxgap < gap :
                 maxgap = gap
                 answer = info2
-                
+
+        
     return answer
