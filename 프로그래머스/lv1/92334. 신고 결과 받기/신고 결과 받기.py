2d008afb5607883ce49e10f5e6ae5a31c6d7d3ca
Jioo Jung <jioome17@gmail.com>
2022-10-25 14:36:02 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,28 +1,21 @@
 from collections import defaultdict
 def solution(id_list, report, k):
+    report= set(report)
     answer = []
-    report = set(report)
-    bad = defaultdict(int)
-    rman = defaultdict(list)
-    badlst = [] 
+    user = defaultdict(list)
+    receive = defaultdict(int)
+    
     for r in report : 
         a,b = r.split(' ')
-        bad[b] += 1 
-        rman[a] += [b]
-    
-    for b,n in bad.items() : 
-        if n>=k : 
-            badlst.append(b)
-    if len(badlst) == 0 :
-        answer = [0]*len(id_list)
-        return answer 
+        user[a].append(b)
+        receive[b] += 1
 
-
-    for i in id_list : 
-        cnt= 0 
-        for a in rman[i]:
-            if a in badlst : 
-                cnt +=1 
-        answer.append(cnt)
-            
-    return answer
+    result = []
+    for id in id_list : 
+        add = 0
+        for j in user[id]:
+            if receive[j] >=k : 
+                add += 1 
+        result.append(add)
+                
+    return result
