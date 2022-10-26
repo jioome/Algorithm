c3e26fcfc2365dc9873597b1556a7e8ab0fe29bc
Jioo Jung <jioome17@gmail.com>
2022-10-26 21:56:21 +0900

    [level 3] Title: 블록 이동하기, Time: 895.24 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
+++ 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
@@ -1,55 +1,60 @@
 from collections import deque
 
-def can_move(cur1, cur2, new_board):
-    x, y = 0, 1
-    cand = []
-    
-    # 평행이동
-    dic = {0:[-1, 0], 1:[1, 0], 2:[0, 1], 3:[0, -1]}
+
+def get_next_pos(pos, board):
+    next_pos = []
+    (x1, y1), (x2, y2) = pos
+    # 상하좌우 이동을 위한 변수선언
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    # 1. 상하좌우 이동
     for i in range(4):
-        nx1 = (cur1[x] + dic[i][0], cur1[y] + dic[i][1])
-        nx2 = (cur2[x] + dic[i][0], cur2[y] + dic[i][1])
-        if new_board[nx1[x]][nx1[y]] == 0 and new_board[nx2[x]][nx2[y]] == 0:
-            cand.append((nx1, nx2))
-            
-    # 회전
-    if cur1[x] == cur2[x]: # 가로방향 일 때
-        UP, DOWN = -1, 1
-        for d in [UP, DOWN]:
-            if new_board[cur1[x]+d][cur1[y]] == 0 and new_board[cur2[x]+d][cur2[y]] == 0:
-                cand.append((cur1, (cur1[x]+d, cur1[y])))
-                cand.append((cur2, (cur2[x]+d, cur2[y])))
-                
-    else: # 세로 방향 일 때
-        LEFT, RIGHT = -1, 1
-        for d in [LEFT, RIGHT]:
-            if new_board[cur1[x]][cur1[y]+d] == 0 and new_board[cur2[x]][cur2[y]+d] == 0:
-                cand.append(((cur1[x], cur1[y]+d), cur1))
-                cand.append(((cur2[x], cur2[y]+d), cur2))
-                
-    return cand
+        if board[x1+dx[i]][y1+dy[i]] == 0 and board[x2+dx[i]][y2+dy[i]] == 0:
+            next_pos.append({(x1+dx[i], y1+dy[i]), (x2+dx[i], y2+dy[i])})
+
+    # 세로 => 가로 회전
+    if y1 == y2:
+        # 왼쪽에서 오른쪽, 오른쪽에서 왼쪽으로 회전하는 경우
+        for i in [-1, 1]:
+            # 오른쪽, 혹은 왼쪽이 둘 다 비어 있다면
+            if board[x1][y1+i] == 0 and board[x2][y2+i] == 0:
+                next_pos.append({(x1, y1), (x1, y1+i)})
+                next_pos.append({(x2, y2), (x2, y2+i)})
+    # 가로 => 세로 회전
+    elif x1 == x2:
+        # 위에서 아래로, 아래에서 위로 회전하는 경우
+        for i in [-1, 1]:
+            # 위쪽 혹은 아래쪽 모두 비어있다면
+            if board[x1+i][y1] == 0 and board[x2+i][y2] == 0:
+                next_pos.append({(x1, y1), (x1+i, y1)})
+                next_pos.append({(x2, y2), (x2+i, y2)})
+    # 다음에 갈 수 있는 위치들 모두 반환
+    return next_pos
+
 
 def solution(board):
-    # board 외벽 둘러싸기
     n = len(board)
-    new_board = [[1] * (n+2) for _ in range(n+2)]
-    for i in range(n):
-        for j in range(n):
-            new_board[i+1][j+1] = board[i][j]
-    
-    for i in new_board:
-        print(i)
-        
-    q = deque( [ ( (1, 1), (1, 2), 0 ) ] ) 
-    # 방문 확인용 
-    check = set( [ ( (1, 1), (1, 2) ) ] )  
-    
+    # 예외처리의 편의를 위하여 주어진 위치 최외곽을 1로 채운 new_board 생성
+    new_board = [[1]*(n+2) for _ in range(n+2)]
+    # 중심부를 board로 대체
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            new_board[i][j] = board[i-1][j-1]
+
+    # BFS Algorithm
+    q = deque()
+    pos = {(1, 1), (1, 2)}
+    visited = []
+    q.append((pos, 0))
+    visited.append(pos)
     while q:
-        cur1, cur2, time = q.popleft()
-        if cur1 == (n, n) or cur2 == (n, n):
-            return time
-        
-        for nxt in can_move(cur1, cur2, new_board):
-            if nxt not in check:
-                q.append((nxt[0], nxt[1], time+1))
-                check.add(nxt)
+        pos, cost = q.popleft()
+        # 종점을 맞닥드리면 cost를 반환하고 종료
+        if (n, n) in pos:
+            return cost
+        # get_next_pos함수를 활용하여 bfs를 수행
+        for next_pos in get_next_pos(pos, new_board):
+            if next_pos not in visited:
+                visited.append(next_pos)
+                q.append((next_pos, cost+1))
