9d6968a956520d8a9f9f6ef017f82e2b21983efd
Jioo Jung <jioome17@gmail.com>
2022-10-26 14:05:41 +0900

    [level 2] Title: 양궁대회, Time: 404.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,27 +1,33 @@
-from itertools import combinations_with_replacement as cwr
 def solution(n, info):
-    answer = [-1]
-    maxgap = -1
-    for com in cwr(range(11),n):
-        info2= [0]*11
-        # 이긴 횟수 
-        lion,apeach = 0,0
-        
-        for i in com : 
-            info2[10-i]+=1
-            
+    global answer,result
+    answer = [0]*(11)
+    result = 0
+    
+    def score(ryan):
+        s = 0
         for i in range(11):
-            if info[i] == info2[i] == 0 : 
+            if ryan[i] == info[i] == 0:
                 continue
-            elif info[i] >= info2[i] : # 어피치가 이김 
-                apeach += 10-i 
-            else : 
-                lion += 10-i
-        if lion > apeach :
-            gap = lion-apeach
-            if maxgap < gap :
-                maxgap = gap
-                answer = info2
-
+            if ryan[i] > info[i]:
+                s += 10 - i
+            else:
+                s -= 10 - i
+        return s
+    def dfs(idx,left,ryan):
+        global result, answer
+        if idx == -1 and left : 
+            return 
+        if left == 0 : 
+            s = score(ryan)
+            if result < s :
+                answer = ryan[:]
+                result = s
+            return 
+        for i in range(left,-1,-1):
+            ryan[idx] = i
+            dfs(idx-1,left-i,ryan)
+            ryan[idx] = 0 
         
-    return answer
+    dfs(10,n,[0 for _ in range(11)])
+    
+    return answer if result != 0 else [-1]
