8fc191d64880133913ab62b35637812b5b3a61b6
Jioo Jung <jioome17@gmail.com>
2022-11-01 16:21:53 +0900

    [level 3] Title: 블록 이동하기, Time: 1006.60 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
+++ 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
@@ -11,35 +11,31 @@ answer = 최소시간
 
 from collections import deque
 
-def get_next_pos(pos, board):
-    next_pos = []
-    (x1, y1), (x2, y2) = pos
-    # 상하좌우 이동을 위한 변수선언
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-
-    # 1. 상하좌우 이동
+def get_next_pos(pos,board):
+    
+    next_pos = [] 
+    (x1,y1), (x2,y2) = pos 
+    dx = [-1,1,0,0]
+    dy = [0,0,1,-1]
+    
+    # 상하좌우
     for i in range(4):
         if board[x1+dx[i]][y1+dy[i]] == 0 and board[x2+dx[i]][y2+dy[i]] == 0:
             next_pos.append({(x1+dx[i], y1+dy[i]), (x2+dx[i], y2+dy[i])})
 
-    # 세로 => 가로 회전
-    if y1 == y2:
-        # 왼쪽에서 오른쪽, 오른쪽에서 왼쪽으로 회전하는 경우
-        for i in [-1, 1]:
-            # 오른쪽, 혹은 왼쪽이 둘 다 비어 있다면
+    # 가로 
+    if x1 == x2 : 
+        for i in [-1,1]:
+            if board[x1+i][y1] == 0 and board[x2+i][y2] == 0 : 
+                next_pos.append({(x1,y1),(x1+i,y1)})
+                next_pos.append({(x2+i,y2),(x2,y2)})
+    
+    # 세로  
+    elif y1 == y2 : 
+        for i in [-1,1]:
             if board[x1][y1+i] == 0 and board[x2][y2+i] == 0:
                 next_pos.append({(x1, y1), (x1, y1+i)})
                 next_pos.append({(x2, y2), (x2, y2+i)})
-    # 가로 => 세로 회전
-    elif x1 == x2:
-        # 위에서 아래로, 아래에서 위로 회전하는 경우
-        for i in [-1, 1]:
-            # 위쪽 혹은 아래쪽 모두 비어있다면
-            if board[x1+i][y1] == 0 and board[x2+i][y2] == 0:
-                next_pos.append({(x1, y1), (x1+i, y1)})
-                next_pos.append({(x2, y2), (x2+i, y2)})
-    # 다음에 갈 수 있는 위치들 모두 반환
     return next_pos
                 
             
