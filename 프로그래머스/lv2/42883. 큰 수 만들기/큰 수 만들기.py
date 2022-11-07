caafccce9fe721e661878139674ac7f2d743951f
Jioo Jung <jioome17@gmail.com>
2022-11-07 18:14:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,14 +1,15 @@
 def solution(number, k):
     answer = ''
     col = [] 
-    for n in number :
-        
-        while len(col) > 0 and col[-1] < n and k>0:
+    
+    for n in number : 
+        while len(col) > 0 and col[-1] < n and k >0 : 
+            
             col.pop()
-            k-= 1
-        
+            k -= 1 
         col.append(n)
-    if k> 0 : 
-        col =col[:-k]
+    if k > 0 : 
+        col = col[:-k]
     answer = ''.join(col)
+            
     return answer
