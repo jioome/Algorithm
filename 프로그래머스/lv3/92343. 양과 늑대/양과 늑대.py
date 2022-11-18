6540583b74bf011bdec61722f54dd46f14552325
Jioo Jung <jioome17@gmail.com>
2022-11-18 16:34:41 +0900

    [level 3] Title: 양과 늑대, Time: 0.34 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,29 +1,30 @@
 def solution(info, edges):
-    # 부모 노드 먼저 방문했나?
-    # 자식 노드 방문한 적이 없나? 
-    answer = [] 
+    
+    answer = []
     visited = [0]*len(info)
-    visited[0] = 1 
+    visited [0] = 1
+    
     
     def dfs(sheep,wolf):
-        if sheep > wolf:
-            answer.append(sheep)
-        else :
+        if sheep <= wolf : 
             return 
-        for i in range(len(edges)):
-            parent = edges[i][0]
-            child = edges[i][1]
-            iswolf = info[child]
-            if visited[parent] == 1 and not visited[child]: 
-                visited[child] = 1 
-                if iswolf == 1 :
+        else : 
+            answer.append(sheep)
+            
+        for p,c in edges : 
+            iswolf = info[c]
+            if visited[p] and not visited[c] : 
+                visited[c] =1 
+                if iswolf : 
                     dfs(sheep,wolf+1)
-                else :
-                    dfs(sheep + 1 , wolf)
-                visited[child] = 0 
-        
-        
-        
+                else : 
+                    dfs(sheep+1,wolf)
+                visited[c] =0
+                
+
+            
+                
+            
     
     dfs(1,0)
     return max(answer)
