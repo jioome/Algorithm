d69225241b253607c826158ae67f84655209ae09
Jioo Jung <jioome17@gmail.com>
2022-11-23 22:00:29 +0900

    [Silver I] Title: 안전 영역, Time: 1280 ms, Memory: 40748 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,35 +1,40 @@
+ 
 import sys
-sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
-n = int(input())
-board = [list(map(int,input().split())) for _ in range(n)]
-high = 0
-result = [] 
-# 제일 큰 값 구하기 
-for i in range(n):
-    high = max(high,max(board[i]))
+sys.setrecursionlimit(100000)
 
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
-def dfs(x,y,r):
-    visited[x][y] = 1
-    dx =[-1,1,0,0]
-    dy =[0,0,1,-1]
-
+def dfs(x,y,h):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and board[nx][ny] > r and visited[nx][ny] == 0 :
-            dfs(nx,ny,r)
+        if -1<nx<n and -1<ny<n and visited[nx][ny] == 0 and graph[nx][ny]>h:
+            visited[nx][ny] = 1
+            dfs(nx,ny,h)
 
-# r은 빗물의 높이 
-for r in range(high):
+n = int(input())
+graph = []
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+
+high = 0
+for i in range(len(graph)):
+    for j in graph[i]:
+        if j > high:
+            high = j
+
+num = 1
+for i in range(high):
     visited = [[0]*n for _ in range(n)]
-    cnt = 0 
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] > r and visited[i][j] == 0 :
-                cnt += 1 
-                dfs(i,j,r)
-    result.append(cnt)
+    cnt = 0
+    for j in range(n):
+        for k in range(n):
+            if graph[j][k] > i and visited[j][k] == 0:
+                cnt += 1
+                visited[j][k] = 1
+                dfs(j,k,i)
+    num = max(num,cnt)
 
-print(max(result))
+print(num)
