101f57e84c0f9b3e6503d2b557ee1a84eda98ca2
Jioo Jung <jioome17@gmail.com>
2022-11-23 20:44:36 +0900

    [level 3] Title: 양과 늑대, Time: 0.35 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,27 +1,24 @@
 def solution(info, edges):
     
-    answer = []
-    visited = [0]*len(info)
-    visited [0] = 1
+    answer = [] 
+    visited = [0]*(len(info))
+    visited[0] = 1 
     
     
     def dfs(sheep,wolf):
-        if sheep <= wolf : 
+        if sheep <= wolf:
             return 
-        else : 
-            answer.append(sheep)
-            
-        for p,c in edges : 
-            iswolf = info[c]
-            if visited[p] and not visited[c] : 
-                visited[c] =1 
+        answer.append(sheep)
+        
+        for p,c in edges:
+            if visited[p] == 1 and visited[c] == 0 : 
+                visited[c] =1
+                iswolf = info[c]
                 if iswolf : 
                     dfs(sheep,wolf+1)
                 else : 
                     dfs(sheep+1,wolf)
-                visited[c] =0
-                
-
+                visited[c] = 0 
             
                 
             
