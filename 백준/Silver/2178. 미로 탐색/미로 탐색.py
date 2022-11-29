b8bf8b1e07a54ba1b140e034b4697f97c2896957
Jioo Jung <jioome17@gmail.com>
2022-11-29 23:44:10 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,12 +1,7 @@
-
 from collections import deque
 n,m  = map(int,input().split())
 board = [list(map(int,input())) for _ in range(n)]
 visited = [[0]*m for _ in range(n)]
-
-# 최소, 최단이라는 말이 나오면 일단 bfs 먼저 생각
-# bfs 이기 때문에 deque를 사용 
-# visitied 배열로 방문 처리 
 
 q = deque()
 def bfs(a,b) : 
@@ -26,15 +21,3 @@ def bfs(a,b) :
 
 bfs(0,0)
 print(board[n-1][m-1])
-
-
-'''
-4 6
-101111
-101010
-101011
-111011
-
-
-=>15
-'''
