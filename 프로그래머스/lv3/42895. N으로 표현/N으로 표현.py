c89fb222d713f2f84acbcc3e045f02b234f2e3e9
Jioo Jung <jioome17@gmail.com>
2022-12-05 22:52:42 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -4,20 +4,23 @@ def solution(N, number):
     s = [set() for _ in range(8)]
 
     # 2. 각 set마다 기본 수 "N" * i 수 초기화
-    for x, a in enumerate( s,start=1 ):
-        a.add(int( str(N) * x ) )
+    for x,a in enumerate(s,1):
+        a.add(int(str(N)*x))
+
     answer = -1
-    for i in range(1, 8):
+    for i in range(1,8):
         for j in range(i):
             for op1 in s[j]:
                 for op2 in s[i-j-1]:
                     s[i].add(op1 + op2)
-                    s[i].add(op1 - op2)
                     s[i].add(op1 * op2)
-                    if op2 != 0:
+                    s[i].add(op1 - op2)
+                    if op2 != 0 : 
                         s[i].add(op1 // op2)
+                    
+    
         if  number in s[i] : 
             return i+1
-
+    
 
     return answer
