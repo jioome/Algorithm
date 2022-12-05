1e441111bda9995f3a5af50c169223bda108d461
Jioo Jung <jioome17@gmail.com>
2022-12-05 22:11:53 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,23 @@
+def solution(N, number):
+    if N == number : 
+        return 1
+    s = [set() for _ in range(8)]
+
+    # 2. 각 set마다 기본 수 "N" * i 수 초기화
+    for x, a in enumerate( s,start=1 ):
+        a.add(int( str(N) * x ) )
+    answer = -1
+    for i in range(1, 8):
+        for j in range(i):
+            for op1 in s[j]:
+                for op2 in s[i-j-1]:
+                    s[i].add(op1 + op2)
+                    s[i].add(op1 - op2)
+                    s[i].add(op1 * op2)
+                    if op2 != 0:
+                        s[i].add(op1 // op2)
+        if  number in s[i] : 
+            return i+1
+
+
+    return answer
