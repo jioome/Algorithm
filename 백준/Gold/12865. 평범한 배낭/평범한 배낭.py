9807778ca4a59a409ac40d812da69ae261c0ed30
Jioo Jung <jioome17@gmail.com>
2022-12-21 01:49:25 +0900

    [Gold V] Title: 평범한 배낭, Time: 5732 ms, Memory: 226336 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,18 @@
 import sys
-input = sys.stdin.readline 
-
+input = sys.stdin.readline
 n,k = map(int,input().split())
-bag = [[0,0]]
+bag = [[0,0]] 
 for i in range(n):
     bag.append(list(map(int,input().split())))
+
 dp = [[0]*(k+1) for _ in range(n+1)]
-for i in range(1,n+1): # 버틸 수 있는 물품 수 
-    for j in range(1,k+1): # 버틸 수 있는 무게
-        w = bag[i][0] # 물건 무게 
-        v = bag[i][1] # 가치
-        if w > j:
+# 물건 수 
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        w = bag[i][0] 
+        v = bag[i][1]
+        if w > j : 
             dp[i][j] = dp[i-1][j]
         else : 
-            dp[i][j] = max(dp[i-1][j-w]+v,dp[i-1][j])
-
+            dp[i][j] = max(dp[i-1][j],dp[i-1][j-w]+v)
 print(dp[n][k])
-
