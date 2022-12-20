23c50d24de8c1c19065b6c9d56c751053708bb9a
Jioo Jung <jioome17@gmail.com>
2022-12-21 01:05:59 +0900

    [Silver II] Title: 이동하기, Time: 844 ms, Memory: 78704 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,12 +1,13 @@
 import sys
+
 input = sys.stdin.readline
 n,m = map(int,input().split())
-board = [list(map(int,input().split())) for _ in range(n)]
+board = [] 
+for i in range(n):
+    board.append(list(map(int,input().split())))
 dp = [[0]*(m+1) for _ in range(n+1)]
 for i in range(1,n+1):
     for j in range(1,m+1):
-        dp[i][j] = max(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]) + board[i-1][j-1]
-
+        dp[i][j] = max(dp[i-1][j] ,dp[i][j-1],dp[i-1][j-1]) + board[i-1][j-1]
 print(dp[n][m])
-
-    
+        
