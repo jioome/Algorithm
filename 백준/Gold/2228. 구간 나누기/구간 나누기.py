c1238379b4ecf7746f8127c8eed5765ccde69631
Jioo Jung <jioome17@gmail.com>
2022-12-30 17:18:05 +0900

    [Gold IV] Title: 구간 나누기, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/2228. 구간 나누기/구간 나누기.py
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.py
@@ -1,3 +1,5 @@
+
+
 import sys
 input = sys.stdin.readline
 
@@ -9,11 +11,9 @@ notcon = [[0]+[-1e9]*m for i in range(n+1)]
 
 for i in range(1, n+1):
     num = int(input())
-    for j in range(1, min(m, (i+1)//2)+1):
-        # i번째 수 미포함 하는 j개 구간합 
-        notcon[i][j]=max(con[i-1][j], notcon[i-1][j])
-        # 구간끼리 겹쳐있을 수 없으니까 notcon[i-1][j-1]
-        con[i][j]=max(con[i-1][j], notcon[i-1][j-1])+num
+    for j in range(1,min(m,(n+1)//2)+1):
+        notcon[i][j]= max(con[i-1][j],notcon[i-1][j])
+        #i 포함
+        con[i][j] = max(con[i-1][j],notcon[i-1][j-1])+ num
 
-# 마지막 원소 포함한 구간 최대값, 마지막 거 뺀 구간합
 print(max(con[n][m], notcon[n][m]))
